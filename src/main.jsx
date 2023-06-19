c9464640b2cb5b963f811045ae665858ceb89967
Wontory <tjddnjsjo@kakao.com>
2023-06-19 23:08:02 +0900

    feat: 라우터 페이지 추가

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
+import Main from "./pages/Main.jsx";
+import PrivacyPolicy from "./pages/PrivacyPolicy.jsx";
 import "./styles/tailwind.css";
 
 const router = createBrowserRouter([
@@ -11,10 +13,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-      { path: "/" },
+      { path: "/", element: <Main /> },
       { path: "/my" },
       { path: "/quiz/:id" },
-      { path: "/privacy" },
+      { path: "/privacy", element: <PrivacyPolicy /> },
       { path: "/404" },
     ],
   },
