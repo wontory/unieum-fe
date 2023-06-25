879574f606f147fcdd3856674680c0142d8f3ffd
Wontory <tjddnjsjo@kakao.com>
2023-06-26 06:33:55 +0900

    design: 디자인시스템 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,44 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          50: "#fafafa",
+          100: "#f5f5f5",
+          200: "#eeeeee",
+          300: "#e0e0e0",
+          400: "#bdbdbd",
+          500: "#9e9e9e",
+          600: "#757575",
+          700: "#616161",
+          800: "#424242",
+          900: "#212121",
+        },
+        slate: {
+          100: "#F4F4FF",
+          200: "#CED5F2",
+          300: "#7B83A9",
+          400: "#141414",
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     themes: [
       {
-        corporate: {
-          ...require("daisyui/src/theming/themes")["[data-theme=corporate]"],
-          accent: "#FEE500",
+        unieum: {
+          primary: "#6d94eb",
+          "primary-content": "#eef3ff",
+          secondary: "#4ebb6c",
+          accent: "#fee500",
+          neutral: "#2b3440",
+          "base-100": "#ffffff",
+          info: "#3abff8",
+          success: "#36d399",
+          warning: "#fbbd23",
+          error: "#f87272",
         },
       },
     ],
