67378c8723080ceb9891da2dadf6e02087bb721f
Wontory <tjddnjsjo@kakao.com>
2023-06-26 06:34:24 +0900

    design: Header, Footer 색 변경

--- src/components/ui/layout/Layout.jsx
+++ src/components/ui/layout/Layout.jsx
@@ -4,11 +4,11 @@ import Navbar from "./Navbar";
 const Layout = ({ children }) => {
   return (
     <>
-      <header className="flex justify-center bg-base-100 border-b border-base-200">
+      <header className="flex justify-center bg-base-100 border-b border-gray-100">
         <Navbar />
       </header>
       <main className="flex justify-center px-4 py-12">{children}</main>
-      <footer className="flex justify-center bg-base-200 p-8">
+      <footer className="flex justify-center bg-gray-100 p-8">
         <Footer />
       </footer>
     </>
