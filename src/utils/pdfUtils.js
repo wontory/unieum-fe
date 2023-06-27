854ba1bef7bd5a15c66f96f01095bfcc58e80237
Wontory <tjddnjsjo@kakao.com>
2023-06-27 17:10:40 +0900

    fix: PDF 한글 깨짐 수정

--- src/utils/pdfUtils.js
+++ src/utils/pdfUtils.js
@@ -4,16 +4,23 @@ import autoTable from "jspdf-autotable";
 
 const generatePdf = (data, isAnswerSheet) => {
   const doc = new jsPDF();
-  const columnWidths = [180];
+
   const tableData = data.flatMap((item) => [
     [item.question],
     [isAnswerSheet ? item.answer : ""],
   ]);
 
+  doc.addFileToVFS("pretendard.ttf", font);
+  doc.addFont("pretendard.ttf", "pretendard", "normal");
+  doc.setFont("pretendard");
+
   autoTable(doc, {
     body: tableData,
     columnStyles: {
-      0: { cellWidth: columnWidths[0] },
+      0: { cellWidth: 180 },
+    },
+    styles: {
+      font: "pretendard",
     },
   });
 
