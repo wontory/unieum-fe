6a6ff89d2ad79b1f91f19a08935853cd74d9731b
Wontory <tjddnjsjo@kakao.com>
2023-06-28 05:32:26 +0900

    design: Breakpoint 변경

--- src/components/ui/FeatureCard.jsx
+++ src/components/ui/FeatureCard.jsx
@@ -1,12 +1,12 @@
 const FeatureCard = ({ children }) => {
   return (
-    <div className="card card-side bg-base-100 shadow-md w-full md:flex-col md:w-100 lg:flex-row">
+    <div className="card card-side bg-base-100 shadow-md w-full md:flex-col md:w-100 xl:flex-row">
       <div className="card-body p-6">
         {children[0]}
         {children[1]}
         {children[2]}
       </div>
-      <figure className="flex-none p-6 pl-0 md:p-6 md:pt-0 lg:p-6 lg:pl-0">
+      <figure className="flex-none p-6 pl-0 md:p-6 md:pt-0 xl:p-6 xl:pl-0">
         {children[3]}
       </figure>
     </div>
