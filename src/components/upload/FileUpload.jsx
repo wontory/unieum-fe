bdb65739a24ddb60217db50faff4c62ba3facf98
Wontory <tjddnjsjo@kakao.com>
2023-06-27 14:03:07 +0900

    feat: 파일 업로드 기능 추가

--- src/components/upload/FileUpload.jsx
+++ src/components/upload/FileUpload.jsx
@@ -6,6 +6,8 @@ import { FilePond, registerPlugin } from "react-filepond";
 import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
 import FilePondPluginPdfPreview from "filepond-plugin-pdf-preview";
 
+import { testApi } from "../../apis/testApi";
+
 import "../../styles/filepond.css";
 import "filepond/dist/filepond.min.css";
 import "filepond-plugin-pdf-preview/dist/filepond-plugin-pdf-preview.min.css";
@@ -17,6 +19,22 @@ const FileUpload = () => {
 
   const [files, setFiles] = useState([]);
 
+  const upload = async (file) => {
+    const formData = new FormData();
+    formData.append("pdf", file);
+
+    const response = await testApi.postPDF(formData);
+
+    localStorage.setItem(
+      "testGenerationId",
+      response.data.data.testGenerationId
+    );
+    localStorage.setItem(
+      "testList",
+      JSON.stringify(response.data.data.testList)
+    );
+  };
+
   return (
     <FilePond
       files={files}
@@ -26,6 +44,9 @@ const FileUpload = () => {
       allowFileTypeValidation={true}
       acceptedFileTypes={["application/pdf"]}
       labelIdle='<span class="filepond--label-action">컴퓨터에서 파일 업로드</span> 또는 여기에 파일을 드롭!'
+      server={{
+        process: (file) => upload(file),
+      }}
       disabled={!ctx.isSignedIn}
     />
   );
