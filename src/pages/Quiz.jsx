2b3fe719ba45bfb4c18415fb1ba28ce7c3ce120d
Wontory <tjddnjsjo@kakao.com>
2023-07-03 12:03:14 +0900

    feat: 퀴즈 페이지 추가

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -4,17 +4,65 @@ import { useParams } from "react-router-dom";
 
 import { testApi } from "../apis/testApi";
 
+import Card from "../components/ui/Card";
+
 const Quiz = () => {
   const { id } = useParams();
 
   const [questions, setQuestions] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [showAnswer, setShowAnswer] = useState(false);
 
   useLayoutEffect(() => {
     testApi.getTest(id).then((res) => {
-      setQuestions(res.data.data.testList.map((e) => JSON.parse(e)));
+      const questionList = res.data.data.testList.map((e) => JSON.parse(e));
+      setQuestions(questionList.sort(() => Math.random() - 0.5));
     });
-    console.log(questions);
-  });
+  }, []);
+
+  return (
+    <Card className="max-w-[1200px] w-full">
+      <progress className="progress progress-primary" value="20" max="100" />
+      <div className="card-title">{questions[currentIndex].question}</div>
+      <div>
+        <textarea
+          className="textarea textarea-bordered w-full"
+          placeholder="정답을 입력해주세요!"
+        />
+      </div>
+      {showAnswer && (
+        <div>
+          <p>AI 정답 (정확하지 않을 수 있습니다!)</p>
+          <div className="alert">
+            <span>{questions[currentIndex].answer}</span>
+          </div>
+        </div>
+      )}
+      <div className="card-actions">
+        {showAnswer ? (
+          <button
+            className="btn btn-primary w-full"
+            onClick={() => {
+              setCurrentIndex((curIndex) => {
+                curIndex + 1;
+              });
+            }}
+          >
+            다음 문제
+          </button>
+        ) : (
+          <button
+            className="btn btn-primary w-full"
+            onClick={() => {
+              setShowAnswer(true);
+            }}
+          >
+            정답 보기
+          </button>
+        )}
+      </div>
+    </Card>
+  );
 };
 
 export default Quiz;
