e1a4bd3b62d94386791deaafc38e8b95f9bf5529
Wontory <tjddnjsjo@kakao.com>
2023-07-03 12:46:48 +0900

    design: 문구 수정

--- src/components/TestList/EmptyList.jsx
+++ src/components/TestList/EmptyList.jsx
@@ -4,7 +4,7 @@ const EmptyList = () => {
   return (
     <div className="card bg-base-100 shadow-md">
       <div className="card-body p-6 items-center text-center">
-        <h2 className="card-title">오류</h2>
+        <h2 className="card-title">알림</h2>
         <p>생성된 문제가 없습니다!</p>
         <div className="card-actions">
           <Link className="btn btn-primary w-full" to="/">
