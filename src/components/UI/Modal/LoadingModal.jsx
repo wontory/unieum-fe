b91ff66984341239e9ffb6a196b87920dd6774f0
Wontory <tjddnjsjo@kakao.com>
2023-07-05 13:41:05 +0900

    refactor: LoadingModal UI 컴포넌트화

--- src/components/UI/Modal/LoadingModal.jsx
+++ src/components/UI/Modal/LoadingModal.jsx
@@ -1,8 +1,8 @@
-const LoadingModal = () => {
+const LoadingModal = ({ children }) => {
   return (
     <dialog id="loading_modal" className="modal">
       <form method="dialog" className="modal-box w-11/12 max-w-5xl">
-        <h3 className="font-bold text-lg">열심히 문제 만드는 중...</h3>
+        <h3 className="font-bold text-lg">{children}</h3>
         <span className="loading loading-spinner loading-lg py-4" />
       </form>
     </dialog>
