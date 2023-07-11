34d48ece2f2836c5dcec91c23f008c7b69569e65
Wontory <tjddnjsjo@kakao.com>
2023-07-11 19:40:20 +0900

    fix: 정답 보기 시 선택지 비활성화

--- src/components/Quiz/MultipleChoiceQuiz.jsx
+++ src/components/Quiz/MultipleChoiceQuiz.jsx
@@ -54,13 +54,14 @@ const MultipleChoiceQuiz = ({ questions }) => {
           max={questions.length}
         />
         <div className="card-title">{questions[currentIndex].question}</div>
-        <ul
-          className="menu menu-lg w-full rounded-box gap-3 p-0"
-          disabled={showAnswer !== 0}
-        >
+        <ul className="menu menu-lg w-full rounded-box gap-3 p-0">
           {questions[currentIndex].options.map((option, index) => (
             <li>
-              <a className="bg-gray-100 p-4" onClick={() => checkAnswer(index)}>
+              <a
+                className="bg-gray-100 p-4"
+                disabled={showAnswer !== 0}
+                onClick={() => checkAnswer(index)}
+              >
                 <p className="text-xl">{circledDigit[index]}</p>
                 <span>{option}</span>
               </a>
