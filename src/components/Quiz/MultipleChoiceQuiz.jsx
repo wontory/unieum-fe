5b85ebab5d6ca543dd7b3e9b17c58ca0ede082fd
Wontory <tjddnjsjo@kakao.com>
2023-07-11 20:47:48 +0900

    fix: 선택지 disabled 수정

--- src/components/Quiz/MultipleChoiceQuiz.jsx
+++ src/components/Quiz/MultipleChoiceQuiz.jsx
@@ -56,8 +56,11 @@ const MultipleChoiceQuiz = ({ questions }) => {
         <div className="card-title">{questions[currentIndex].question}</div>
         <ul className="menu menu-lg w-full rounded-box gap-3 p-0">
           {questions[currentIndex].options.map((option, index) => (
-            <li className={showAnswer !== 0 && "disabled"}>
-              <a className="bg-gray-100 p-4" onClick={() => checkAnswer(index)}>
+            <li
+              className={showAnswer !== 0 && "disabled"}
+              onClick={() => checkAnswer(index)}
+            >
+              <a className="bg-gray-100 p-4">
                 <p className="text-xl">{circledDigit[index]}</p>
                 <span>{option}</span>
               </a>
