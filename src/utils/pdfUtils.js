0fc38c1ccc39fb030395f73d3177849d974c14d8
Wontory <tjddnjsjo@kakao.com>
2023-07-12 01:16:50 +0900

    test: 객관식 pdf 테스트

--- src/utils/pdfUtils.js
+++ src/utils/pdfUtils.js
@@ -11,7 +11,11 @@ const generatePdf = (data, testFormat, isAnswerSheet) => {
   const tableData =
     testFormat === "multiple-choice"
       ? data.flatMap((item) => [
-          [item.question + isAnswerSheet ? ` ... ${item.answer}` : ""],
+          [
+            `${item.question}${
+              isAnswerSheet ? ` ... ${Number(item.answer) + 1}` : ""
+            }`,
+          ],
           [
             item.options
               .map((option, index) => `${index + 1} ${option}`)
